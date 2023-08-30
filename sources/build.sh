fadc14a9e36c4913caf983861b697d5334081c9c
Emma Marichal <bonjour@emmamarichal.fr>
2023-08-30 14:01:44 +0200

    Small export fixes

--- sources/build.sh
+++ sources/build.sh
@@ -8,4 +8,7 @@ gftools builder config.yaml
 echo "Build BioRhyme Expanded"
 fonttools varLib.instancer "../fonts/variable/BioRhyme[wdth,wght].ttf" wdth=200 --update-name-table -o "../fonts/variable/BioRhymeExpanded[wght].ttf"
 
+echo "Fix instance"
+gftools fix-instances ../fonts/variable/BioRhymeExpanded[wght].ttf --inplace
+
 echo "Complete"
